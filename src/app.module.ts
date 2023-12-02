0018400ad4c171895619ad1e64491b2bc6e0ddfe
tolluset <dlqud19@gmail.com>
2023-12-02 17:19:21 +0900

    feat: add health-check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthCheckController } from './health-check/health-check.controller';
+import { HealthCheckService } from './health-check/health-check.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, HealthCheckController],
+  providers: [AppService, HealthCheckService],
 })
 export class AppModule {}
