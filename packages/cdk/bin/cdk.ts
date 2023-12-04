ac13715e859424b832dbed44a2d76100bf56241e
tolluset <dlqud19@gmail.com>
2023-12-05 02:33:14 +0900

    chore: add ecr stack in bin

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { CdkStack } from "../lib/cdk-stack";
+import { EcrStack } from "../lib/ecr-stack";
 
 const app = new cdk.App();
+
 new CdkStack(app, "CdkStack");
+new EcrStack(app, "EcrStack");
