25a0e0063d3991ebea98ac4b9303de835a5f05a3
tolluset <dlqud19@gmail.com>
2023-12-05 02:33:00 +0900

    chore: add vpc

--- packages/cdk/lib/cdk-stack.ts
+++ packages/cdk/lib/cdk-stack.ts
@@ -1,16 +1,16 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { aws_ec2 as ec2, aws_ecr as ecr } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { env_name } from "../env";
+import { Stack, StackProps, aws_ec2 as ec2, aws_ecr as ecr } from "aws-cdk-lib";
 
 const PUBLIC_ROUTE = "0.0.0.0/0";
 const VPC_CIDR = "10.100.0.0/16";
+
+const HTTP = 80;
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, `${id}-Vpc`, {
+    const _vpc = new ec2.Vpc(this, `${id}-Vpc`, {
       ipAddresses: ec2.IpAddresses.cidr(VPC_CIDR),
       maxAzs: 2,
       subnetConfiguration: [
@@ -31,23 +31,26 @@ export class CdkStack extends Stack {
       this,
       `${id}-PublicRouteTable`,
       {
-        vpcId: vpc.vpcId,
+        vpcId: _vpc.vpcId,
       },
     );
 
     new ec2.CfnRoute(this, `${id}-IgwRoute`, {
       routeTableId: publicRouteTable.ref,
       destinationCidrBlock: PUBLIC_ROUTE,
-      gatewayId: vpc.internetGatewayId,
+      gatewayId: _vpc.internetGatewayId,
     });
 
-    vpc.addInterfaceEndpoint(`${id}-EcrEndpointForPrivate`, {
+    _vpc.addInterfaceEndpoint(`${id}-EcrEndpointForPrivate`, {
       service: ec2.InterfaceVpcEndpointAwsService.ECR,
       subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
     });
 
-    new ecr.Repository(this, `${id}-NestRepository`, {
-      repositoryName: `nest-${env_name}`,
+    const albSG = new ec2.SecurityGroup(this, `${id}-albSG`, {
+      vpc: _vpc,
+      allowAllOutbound: false,
     });
+
+    albSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(HTTP));
   }
 }
