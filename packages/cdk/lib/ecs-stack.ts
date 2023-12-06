3ceb7a4764ce3de924965726dbed914d4d68efbc
tolluset <dlqud19@gmail.com>
2023-12-07 02:12:02 +0900

    chore: add fargate task definition

--- packages/cdk/lib/ecs-stack.ts
+++ packages/cdk/lib/ecs-stack.ts
@@ -20,19 +20,30 @@ export class ECSStack extends Stack {
 
     const repository = ecr.Repository.fromRepositoryName(
       this,
-      `${id}_ecs_from_repository_name`,
+      `${id}-ecs_from_repository_name`,
       `nest-${envName}`,
+    );
+
+    const fargateTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      `${id}-ecs_patterns_faragate_task`,
+    );
+
+    fargateTaskDefinition.addContainer(
+      `${id}-ecs_patterns_faragate_task_container`,
+      {
+        image: ecs.ContainerImage.fromEcrRepository(repository, imageDigest),
+        portMappings: [{ containerPort: 3000 }],
+      },
     );
 
     const service = new ecs_patterns.ApplicationLoadBalancedFargateService(
       this,
       `${id}_ecs_patterns_ecs`,
+      {
+        taskDefinition: fargateTaskDefinition,
+      },
     );
-
-    service.taskDefinition.addContainer(`${id}-addContainer`, {
-      image: ecs.ContainerImage.fromEcrRepository(repository, imageDigest),
-      portMappings: [{ containerPort: 3000 }],
-    });
 
     service.targetGroup.configureHealthCheck({
       path: "/health-check",
