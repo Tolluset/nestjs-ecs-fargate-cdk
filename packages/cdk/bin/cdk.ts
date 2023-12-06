42f9d554a31819312ab09350c2f2c32e848f98a7
tolluset <dlqud19@gmail.com>
2023-12-06 23:56:38 +0900

    chore: add ecs by ecs_patterns [ci skip]

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -2,8 +2,11 @@
 import * as cdk from "aws-cdk-lib";
 import { CdkStack } from "../lib/cdk-stack";
 import { EcrStack } from "../lib/ecr-stack";
+import { ECSStack } from "../lib/ecs-stack";
 
 const app = new cdk.App();
 
 new CdkStack(app, "CdkStack");
+
+new ECSStack(app, "EcsStack");
 new EcrStack(app, "EcrStack");
---
+++ packages/cdk/lib/ecs-stack.ts
@@ -0,0 +1,38 @@
+import { Construct } from "constructs";
+import {
+  Stack,
+  StackProps,
+  aws_ecs_patterns as ecs_patterns,
+  aws_ecr as ecr,
+  aws_ecs as ecs,
+} from "aws-cdk-lib";
+
+export class ECSStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const repository = ecr.Repository.fromRepositoryName(
+      this,
+      `${id}_ecs_from_repository_name`,
+      "nest-dev",
+    );
+
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      `${id}_ecs_patterns_ecs`,
+      {
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromEcrRepository(
+            repository,
+            "e2fffbeb71a3eb62e8aa13c3f70f821ec52886d5",
+          ),
+        },
+        // publicLoadBalancer: true,
+      },
+    );
+
+    service.targetGroup.configureHealthCheck({
+      path: "/health-check",
+    });
+  }
+}
